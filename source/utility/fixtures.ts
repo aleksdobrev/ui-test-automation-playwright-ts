23886ed2db2b34e8262da8c21ce1b45f08e11080
aleksdobrev <aleksdobrev87@gmail.com>
2026-04-09 16:19:07 +0300

    Create Products List page fixture

--- source/utility/fixtures.ts
+++ source/utility/fixtures.ts
@@ -1,13 +1,19 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '@page-objects/LoginPage';
+import { ProductsListPage } from '@page-objects/ProductsListPage';
 
 interface Fixtures {
   loginPage: LoginPage;
+  productsListPage: ProductsListPage;
 }
 
 export const test = base.extend<Fixtures>({
   loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
+  },
+
+  productsListPage: async ({ page }, use) => {
+    await use(new ProductsListPage(page));
   },
 });
 
