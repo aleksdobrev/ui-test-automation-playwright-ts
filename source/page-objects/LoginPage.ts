11bad926f3c068aa28b46d350a27daabb65a1872
aleksdobrev <aleksdobrev87@gmail.com>
2026-04-09 15:58:36 +0300

    Add signInUser method

--- source/page-objects/LoginPage.ts
+++ source/page-objects/LoginPage.ts
@@ -5,11 +5,29 @@ export class LoginPage {
   readonly page: Page;
   readonly loginPageUrl: string;
   readonly loginForm: Locator;
+  readonly userNameInputField: Locator;
+  readonly passwordInputField: Locator;
+  readonly loginButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.loginPageUrl = '/';
     this.loginForm = page.locator('#login_button_container');
+    this.userNameInputField = page.locator('[data-test="username"]');
+    this.passwordInputField = page.locator('[data-test="password"]');
+    this.loginButton = page.locator('[data-test="login-button"]');
+  }
+
+  /**
+   * Sign in a user with the provided username and password.
+   * @param username - The username of the user to sign in.
+   * @param password - The password of the user to sign in.
+   */
+  async signInUser(username: string, password: string) {
+    await this.userNameInputField.fill(username);
+    await this.passwordInputField.fill(password);
+    await this.loginButton.click();
+    // TODO helper methods for click and fill.
   }
 
   /**
