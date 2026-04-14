465460f53fb7d9e8cc09e846799109d6de51c7ea
aleksdobrev <aleksdobrev87@gmail.com>
2026-04-14 16:16:59 +0300

    Add shoppingCartPage fixture

--- source/utility/fixtures.ts
+++ source/utility/fixtures.ts
@@ -2,11 +2,13 @@ import { test as base } from '@playwright/test';
 import { LoginPage } from '@page-objects/LoginPage';
 import { ProductsListPage } from '@page-objects/ProductsListPage';
 import { ProductDetailsPage } from '@page-objects/ProductDetailsPage';
+import { ShoppingCartPage } from '@page-objects/ShoppingCartPage';
 
 interface Fixtures {
   loginPage: LoginPage;
-  productsListPage: ProductsListPage;
   productDetailsPage: ProductDetailsPage;
+  productsListPage: ProductsListPage;
+  shoppingCartPage: ShoppingCartPage;
 }
 
 export const test = base.extend<Fixtures>({
@@ -20,6 +22,10 @@ export const test = base.extend<Fixtures>({
 
   productsListPage: async ({ page }, use) => {
     await use(new ProductsListPage(page));
+  },
+
+  shoppingCartPage: async ({ page }, use) => {
+    await use(new ShoppingCartPage(page));
   },
 });
 
